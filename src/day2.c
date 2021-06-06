d061945ddb38127dbc418290ebe6570f31452073
Michael Hadley <mikethadley@gmail.com>
2021-06-06 14:41:06 -0700

    Take arg for which day to run

---
+++ src/day2.c
@@ -0,0 +1,8 @@
+#include "day.h"
+#include "day2.h"
+
+struct Day day2() {
+  struct Day day = {};
+
+  return day;
+}
--- src/main.c
+++ src/main.c
@@ -1,48 +1,74 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "day1.h"
+#include "day2.h"
 #include "day.h"
 
-#define MAX_DAYS 1
+#define MAX_DAYS 2
 
 typedef struct Day (*DAY_FN)(void);
 
 const DAY_FN days[MAX_DAYS] = {
-  day1
+  day1,
+  day2
 };
 
 void print_part(const struct Answer* answer, int part);
 void run_day(int day);
 
-int main() {
-  for (int i = 0; i < MAX_DAYS; i++) {
-    run_day(i);
+int main(int argc, char* argv[]) {
+  int day_to_run = 0;
+  char opt;
 
-    if (i + 1 < MAX_DAYS) puts("\n");
+  while ((opt = getopt(argc, argv, "d:")) != -1) {
+    switch (opt) {
+      case 'd':
+        day_to_run = atoi(optarg);
+        break;
+    }
+  }
+
+  if (day_to_run > 0) {
+    if (day_to_run > MAX_DAYS) {
+      fprintf(stderr, "Day number cannot be greater than %i\n", MAX_DAYS);
+      exit(1);
+    }
+
+    run_day(day_to_run);
+  } else {
+    for (int i = 0; i < MAX_DAYS; i++) {
+      run_day(i + 1);
+
+      if (i != MAX_DAYS - 1) puts("");
+    }
   }
 
   return 0;
 }
 
 void run_day(int day_n) {
-  struct Day day = days[day_n]();
+  struct Day day = days[day_n - 1]();
 
-  printf("Day %i\n", day_n + 1);
+  printf("Day %i\n", day_n);
   puts("================");
   print_part(&day.part1, 1);
   print_part(&day.part2, 2);
 }
 
 void print_part(const struct Answer* answer, int part) {
-  printf("Part %i: %zu ", part, answer->actual);
+  char* sym;
 
   if (!answer->expected) {
-    puts("❓");
+    sym = "❓";
   } else {
     if (answer->expected == answer->actual) {
-      puts("✅");
+      sym = "✅";
     } else {
-      puts("❌");
+      sym = "❌";
     }
   }
+
+  printf("Part %i: %zu %s\n", part, answer->actual, sym);
 }
