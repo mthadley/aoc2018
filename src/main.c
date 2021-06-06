242fbc08561328162164ce448ddd24e13f4cc723
Michael Hadley <mikethadley@gmail.com>
2021-06-06 14:09:36 -0700

    Run all days by default

--- src/main.c
+++ src/main.c
@@ -3,17 +3,34 @@
 #include "day1.h"
 #include "day.h"
 
+#define MAX_DAYS 1
+
+typedef struct Day (*DAY_FN)(void);
+
+const DAY_FN days[MAX_DAYS] = {
+  day1
+};
+
 void print_part(const struct Answer* answer, int part);
+void run_day(int day);
 
 int main() {
-  struct Day day = day1();
+  for (int i = 0; i < MAX_DAYS; i++) {
+    run_day(i);
 
-  puts("Answer for Day 1");
+    if (i + 1 < MAX_DAYS) puts("\n");
+  }
+
+  return 0;
+}
+
+void run_day(int day_n) {
+  struct Day day = days[day_n]();
+
+  printf("Day %i\n", day_n + 1);
   puts("================");
   print_part(&day.part1, 1);
   print_part(&day.part2, 2);
-
-  return 0;
 }
 
 void print_part(const struct Answer* answer, int part) {
