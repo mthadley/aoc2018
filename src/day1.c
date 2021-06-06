578e41da2929acf4d6369be47a7785de6c936885
Michael Hadley <mikethadley@gmail.com>
2021-06-06 12:36:44 -0700

    Combine both parts

--- src/day1.c
+++ src/day1.c
@@ -7,35 +7,17 @@
 #include "day1.h"
 #include "../input/day1.h"
 
-static size_t part1();
-static size_t part2();
-
 struct Day day1() {
-  return (struct Day){
-    .part1 = { .actual = part1(), .expected = 400 },
-    .part2 = { .actual = part2(), .expected = 232 }
+  struct Day day = {
+    .part1 = { .expected = 400 },
+    .part2 = { .expected = 232 }
   };
-}
 
-static size_t part1() {
   size_t total = 0;
+  bool first_pass = true;
 
-  char** lines = g_strsplit((gchar*)input_day1_txt, "\n", 0);
-  char* line;
-
-  for (int line_n = 0; (line = lines[line_n]); line_n++) {
-    if (line[0] == '\0') continue;
-
-    total += atoi(line);
-  }
-
-  return total;
-}
-
-static size_t part2() {
   GHashTable* seen = g_hash_table_new(g_int_hash, g_int_equal);
   char** lines = g_strsplit((gchar*)input_day1_txt, "\n", 0);
-  size_t total = 0;
 
   while (true) {
     char* line;
@@ -46,12 +28,17 @@ static size_t part2() {
       total += atoi(line);
 
       if (g_hash_table_lookup_extended(seen, &total, NULL, NULL)) {
-        return total;
+        day.part2.actual = total;
+
+        return day;
       } else {
         g_hash_table_insert(seen, &total, NULL);
       }
     }
-  }
 
-  return 0;
+    if (first_pass) {
+      day.part1.actual = total;
+      first_pass = false;
+    }
+  }
 }
