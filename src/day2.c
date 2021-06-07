dc5537635d254afa42f40f06ea34299b9347c15b
Michael Hadley <mikethadley@gmail.com>
2021-06-07 13:17:00 -0700

    Day 2 part 1

--- src/day2.c
+++ src/day2.c
@@ -1,8 +1,76 @@
+#include <stdbool.h>
+#include <glib.h>
+#include <stdio.h>
+
 #include "day.h"
 #include "day2.h"
+#include "../input/day2.h"
+
+struct CountState {
+  bool counted_two;
+  int two_count;
+  bool counted_three;
+  int three_count;
+};
+
+static void count_occurrences(gpointer key_p __attribute__((unused)),
+                              gpointer count_p,
+                              gpointer cs_p) {
+  unsigned int* count = count_p;
+  struct CountState* cs = cs_p;
+
+  if (*count == 2 && !cs->counted_two) {
+    cs->two_count++;
+    cs->counted_two = true;
+  }
+
+  if (*count == 3 && !cs->counted_three) {
+    cs->three_count++;
+    cs->counted_three = true;
+  }
+}
 
 struct Day day2() {
-  struct Day day = {};
+  struct Day day = {
+    .part1 = { .expected = 5166 }
+  };
+
+  unsigned int with_two = 0;
+  unsigned int with_three = 0;
+
+  char** lines = g_strsplit((gchar*)input_day2_txt, "\n", 0);
+
+  char* line;
+  for (int line_n = 0; (line = lines[line_n]); line_n++) {
+    GHashTable* seen = g_hash_table_new(g_int_hash, g_int_equal);
+
+    char c;
+    for (int char_n = 0; (c = line[char_n]); char_n++) {
+      int* current_count = g_hash_table_lookup(seen, &c);
+
+      if (current_count == NULL) {
+        unsigned int* count_p = malloc(sizeof(int));
+        *count_p = 1;
+
+        g_hash_table_insert(seen, &c, count_p);
+      } else {
+        (*current_count)++;
+      };
+    }
+
+    struct CountState cs = {
+      .counted_two = false,
+      .two_count = 0,
+      .counted_three = false,
+      .three_count = 0,
+    };
+    g_hash_table_foreach(seen, count_occurrences, &cs);
+
+    with_two += cs.two_count;
+    with_three += cs.three_count;
+  }
+
+  day.part1.actual = with_two * with_three;
 
   return day;
 }
