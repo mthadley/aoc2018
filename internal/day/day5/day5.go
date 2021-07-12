de7cbec0a4eaae378f2afc9e9abdb5fe9d7220d0
Michael Hadley <mikethadley@gmail.com>
2021-07-11 19:47:47 -0700

    Optimize

--- internal/day/day5/day5.go
+++ internal/day/day5/day5.go
@@ -23,18 +23,32 @@ func part1(line string) string {
 }
 
 func react(line string) string {
-	for i, r := range line {
+	i := 0
+	prev := 0
+	result := ""
+
+	for i < len(line) {
 		if i+1 >= len(line) {
-			continue
+			result += line[prev : i+1]
+			break
 		}
 
-		otherR := line[i+1]
-		if reactsWith(r, rune(otherR)) {
-			return react(line[:i] + line[i+2:])
+		r, otherR := rune(line[i]), rune(line[i+1])
+
+		if reactsWith(r, otherR) {
+			result += line[prev:i]
+			i += 2
+			prev = i
+		} else {
+			i++
 		}
 	}
 
-	return line
+	if len(result) < len(line) {
+		return react(result)
+	}
+
+	return result
 }
 
 func reactsWith(r, otherR rune) bool {
